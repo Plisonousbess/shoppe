efcaf65ea88a025c421840a40c6e1c7662684abe
Maurice Jeffery & Andrew Dowd <pair+lavalampmj.plisonousbess@devbootcamp.com>
2015-10-20 12:23:28 -0700

    add CRUD without using scaffold

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+       render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+      if @product.update(product_params)
+         redirect_to @product, notice: 'Product was successfully updated.'
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_url, notice: 'Product was successfully destroyed.'
+  end
+
+private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
+
+end
